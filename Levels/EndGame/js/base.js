01e88efbdcc06f960883d67bc108d797de491b78
Alviss0 <91151004+Alviss0@users.noreply.github.com>
2022-01-20 09:19:51 +0300

    Delete Levels directory

--- Levels/EndGame/js/base.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import { changeTheme } from "../../../Common/Theme/ThemeJS/changeTheme.js";
-import { CountingResults } from "./CountingResults.js";
-import { addRetry } from "./addRetry.js";
-
-CountingResults();
-addRetry();
-changeTheme();
--- Levels/Main/js/base.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import { changeTheme } from "../../../Common/Theme/ThemeJS/changeTheme.js";
-import { createLifes } from "../../../Common/Lifes/createLifes.js";
-import { addStart } from "./Start.js";
-
-createLifes();
-changeTheme();
-addStart();
